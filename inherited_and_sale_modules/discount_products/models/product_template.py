13be5391def750be3c6b933ebc4c3e3ead3eb9d7
smuhammadtaha3 <taha771871@gmail.com>
2025-07-01 13:08:09 +0500

    deleting

--- inherited_and_sale_modules/discount_products/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    # _inherit = 'sale.order.line'
-
-    custom_discount = fields.Float(string='Discount (%)', default=0.0, help="Discount percentage for the product.")
--- travels_management/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import travels_management
